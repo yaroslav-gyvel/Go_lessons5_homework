c8405c5c861fa9650bfbed195239766dbf8d0332
Yaroslav Gyvel <yaroslav.hyvel@gmail.com>
2022-06-15 01:53:18 +0300

    1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_lessons5_homework.git
+
+go 1.18
--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 )
 
 func main() {
-	// const (
-	// 	a = 1
-	// 	b = "hello"
-	// 	c = true
-	// 	e = 1234.3456
-	// )
-	s2 = "привіт"
-	fmt.Println("s2", s2)
+	const (
+		priceApple = 5.99
+		pricePear  = 7.00
+		b          = "hello"
+		c          = true
+		e          = 1234.3456
+	)
+	fmt.Println("Hello")
 }

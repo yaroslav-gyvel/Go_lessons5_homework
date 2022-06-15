e0c069198045391b42dc46602dce22409565a8cd
Yaroslav Gyvel <yaroslav.hyvel@gmail.com>
2022-06-15 06:21:36 +0300

    finish

--- main.go
+++ main.go
@@ -2,12 +2,44 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
+
 	const (
-		priceApple = 5.99
-		pricePear  = 7
+		priceOneApple = 5.99
+		priceOnePear  = 7
+		haveMoney     = 23
+		countApples   = 9
+		countPears    = 8
 	)
-	fmt.Println("Hello")
+
+	var (
+		costApples float64
+		costPears  float64
+		sumMoney   float64
+	)
+
+	fmt.Println("Price one apple", priceOneApple)
+	fmt.Println("Price one pear", priceOnePear)
+	fmt.Println("We have money", haveMoney)
+	fmt.Println("")
+	costApples = countApples * priceOneApple
+	costPears = countPears * priceOnePear
+	sumMoney = costApples + costPears
+	fmt.Println("How much money do you have to spend to buy 9 apples and 8 pears?", sumMoney)
+	fmt.Println("")
+	buyPears := float64(haveMoney) / float64(priceOnePear)
+	int, frac := math.Modf(buyPears)
+	_ = frac
+	fmt.Println("How many pears can we buy?", int)
+	fmt.Println("")
+	buyApples := float64(haveMoney) / float64(priceOneApple)
+	i, f := math.Modf(buyApples)
+	_ = f
+	fmt.Println("How many apples can we buy?", i)
+	fmt.Println("")
+	sumMoney = 2*priceOneApple + 2*priceOnePear
+	fmt.Println("Can we buy 2 pears and 2 apples?", sumMoney < float64(haveMoney))
 }

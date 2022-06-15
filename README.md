360852462a3b25bff51134a8535409dd8aaf3980
Yaroslav Gyvel <yaroslav.hyvel@gmail.com>
2022-06-15 06:28:38 +0300

    end

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Go_lessons5_homework
+
+// One apple costs UAH 5.99. The price of one pear is UAH 7.
+// We have UAH 23.
+// 1. How much money do you have to spend to buy 9 apples and 8 pears?
+// 2. How many pears can we buy?
+// 3. How many apples can we buy?
+// 4. Can we buy 2 pears and 2 apples?
+//
--- main.go
+++ main.go
@@ -21,24 +21,24 @@ func main() {
 		sumMoney   float64
 	)
 
-	fmt.Println("Price one apple", priceOneApple)
-	fmt.Println("Price one pear", priceOnePear)
-	fmt.Println("We have money", haveMoney)
+	fmt.Println("Price one apple", priceOneApple, "uah")
+	fmt.Println("Price one pear", priceOnePear, "uah")
+	fmt.Println("We have money", haveMoney, "uah")
 	fmt.Println("")
 	costApples = countApples * priceOneApple
 	costPears = countPears * priceOnePear
 	sumMoney = costApples + costPears
-	fmt.Println("How much money do you have to spend to buy 9 apples and 8 pears?", sumMoney)
+	fmt.Println("How much money do you have to spend to buy 9 apples and 8 pears?", sumMoney, "uah")
 	fmt.Println("")
 	buyPears := float64(haveMoney) / float64(priceOnePear)
 	int, frac := math.Modf(buyPears)
 	_ = frac
-	fmt.Println("How many pears can we buy?", int)
+	fmt.Println("How many pears can we buy?", int, "pieces")
 	fmt.Println("")
 	buyApples := float64(haveMoney) / float64(priceOneApple)
 	i, f := math.Modf(buyApples)
 	_ = f
-	fmt.Println("How many apples can we buy?", i)
+	fmt.Println("How many apples can we buy?", i, "pieces")
 	fmt.Println("")
 	sumMoney = 2*priceOneApple + 2*priceOnePear
 	fmt.Println("Can we buy 2 pears and 2 apples?", sumMoney < float64(haveMoney))
